e9787677078b0f67cf28cb69c5b4884446b8977a
marish11 <marishokola89@gmail.com>
2022-05-02 14:23:48 +0300

    feat: add ru and en layouts

---
+++ virtual-keyboard/layouts.js
@@ -0,0 +1,647 @@
+const en = [
+  {
+    key: '`',
+    shiftKey: '~',
+    code: 'Backquote'
+  },
+  {
+    key: '1',
+    shiftKey: '!',
+    code: 'Digit1'
+  },
+  {
+    key: '2',
+    shiftKey: '@',
+    code: 'Digit2'
+  },
+  {
+    key: '3',
+    shiftKey: '#',
+    code: 'Digit3'
+  },
+  {
+    key: '4',
+    shiftKey: '$',
+    code: 'Digit4'
+  },
+  {
+    key: '5',
+    shiftKey: '%',
+    code: 'Digit5'
+  },
+  {
+    key: '6',
+    shiftKey: '^',
+    code: 'Digit6'
+  },
+  {
+    key: '7',
+    shiftKey: '&',
+    code: 'Digit7'
+  },
+  {
+    key: '8',
+    shiftKey: '*',
+    code: 'Digit8'
+  },
+  {
+    key: '9',
+    shiftKey: '(',
+    code: 'Digit9'
+  },
+  {
+    key: '0',
+    shiftKey: ')',
+    code: 'Digit0'
+  },
+  {
+    key: '-',
+    shiftKey: '_',
+    code: 'Minus'
+  },
+  {
+    key: '=',
+    shiftKey: '+',
+    code: 'Equal'
+  },
+  {
+    key: 'Backspace',
+    shiftKey: null,
+    code: 'Backspace'
+  },
+  {
+    key: 'Tab',
+    shiftKey: null,
+    code: 'Tab'
+  },
+  {
+    key: 'q',
+    shiftKey: 'Q',
+    code: 'LetterQ'
+  },
+  {
+    key: 'w',
+    shiftKey: 'W',
+    code: 'LetterW'
+  },
+  {
+    key: 'e',
+    shiftKey: 'E',
+    code: 'LetterE'
+  },
+  {
+    key: 'r',
+    shiftKey: 'R',
+    code: 'LetterR'
+  },
+  {
+    key: 't',
+    shiftKey: 'T',
+    code: 'LetterT'
+  },
+  {
+    key: 'y',
+    shiftKey: 'Y',
+    code: 'LetterY'
+  },
+  {
+    key: 'u',
+    shiftKey: 'U',
+    code: 'LetterU'
+  },
+  {
+    key: 'i',
+    shiftKey: 'I',
+    code: 'LetterI'
+  },
+  {
+    key: 'o',
+    shiftKey: 'O',
+    code: 'LetterO'
+  },
+  {
+    key: 'p',
+    shiftKey: 'P',
+    code: 'LetterP'
+  },
+  {
+    key: '[',
+    shiftKey: '{',
+    code: 'OpenBracket'
+  },
+  {
+    key: ']',
+    shiftKey: '}',
+    code: 'CloseBracket'
+  },
+  {
+    key: '\\',
+    shiftKey: '|',
+    code: 'Backslash'
+  },
+  {
+    key: 'Del',
+    shiftKey: null,
+    code: 'Delete'
+  },
+  {
+    key: 'Caps Lock',
+    shiftKey: null,
+    code: 'CapsLock'
+  },
+  {
+    key: 'a',
+    shiftKey: 'A',
+    code: 'LetterA'
+  },
+  {
+    key: 's',
+    shiftKey: 'S',
+    code: 'LetterS'
+  },
+  {
+    key: 'd',
+    shiftKey: 'D',
+    code: 'LetterD'
+  },
+  {
+    key: 'f',
+    shiftKey: 'F',
+    code: 'LetterF'
+  },
+  {
+    key: 'g',
+    shiftKey: 'G',
+    code: 'LetterG'
+  },
+  {
+    key: 'h',
+    shiftKey: 'H',
+    code: 'LetterH'
+  },
+  {
+    key: 'j',
+    shiftKey: 'J',
+    code: 'LetterJ'
+  },
+  {
+    key: 'k',
+    shiftKey: 'K',
+    code: 'LetterK'
+  },
+  {
+    key: 'l',
+    shiftKey: 'L',
+    code: 'LetterL'
+  },
+  {
+    key: ';',
+    shiftKey: ':',
+    code: 'Semicolon'
+  },
+  {
+    key: "'",
+    shiftKey: '"',
+    code: 'Quote'
+  },
+  {
+    key: 'Enter',
+    shiftKey: null,
+    code: 'Enter'
+  },
+  {
+    key: 'Shift',
+    shiftKey: null,
+    code: 'LeftShift'
+  },
+  {
+    key: 'z',
+    shiftKey: 'Z',
+    code: 'LetterZ'
+  },
+  {
+    key: 'x',
+    shiftKey: 'X',
+    code: 'LetterX'
+  },
+  {
+    key: 'c',
+    shiftKey: 'C',
+    code: 'LetterC'
+  },
+  {
+    key: 'v',
+    shiftKey: 'V',
+    code: 'LetterV'
+  },
+  {
+    key: 'b',
+    shiftKey: 'B',
+    code: 'LetterB'
+  },
+  {
+    key: 'n',
+    shiftKey: 'N',
+    code: 'LetterN'
+  },
+  {
+    key: 'm',
+    shiftKey: 'M',
+    code: 'LetterM'
+  },
+  {
+    key: ',',
+    shiftKey: '<',
+    code: 'Comma'
+  },
+  {
+    key: '.',
+    shiftKey: '>',
+    code: 'Period'
+  },
+  {
+    key: '/',
+    shiftKey: '?',
+    code: 'ForwardSlash'
+  },
+  {
+    key: '&uarr;',
+    shiftKey: null,
+    code: 'ArrowUp'
+  },
+  {
+    key: 'Shift',
+    shiftKey: null,
+    code: 'RightShift'
+  },
+  {
+    key: 'Ctrl',
+    shiftKey: null,
+    code: 'LeftCtrl'
+  },
+  {
+    key: 'Win',
+    shiftKey: null,
+    code: 'Win'
+  },
+  {
+    key: 'Alt',
+    shiftKey: null,
+    code: 'LeftAlt'
+  },
+  {
+    key: ' ',
+    shiftKey: null,
+    code: 'Space'
+  },
+  {
+    key: 'Alt',
+    shiftKey: null,
+    code: 'RightAlt'
+  },
+  {
+    key: 'Ctrl',
+    shiftKey: null,
+    code: 'RightCtrl'
+  },
+  {
+    key: '&larr;',
+    shiftKey: null,
+    code: 'ArrowLeft'
+  },
+  {
+    key: '&darr;',
+    shiftKey: null,
+    code: 'ArrowDown'
+  },
+  {
+    key: '&rarr;',
+    shiftKey: null,
+    code: 'ArrowRight'
+  },
+];
+
+const ru = [
+  {
+    key: 'ё',
+    shiftKey: 'Ё',
+    code: 'Backquote'
+  },
+  {
+    key: '1',
+    shiftKey: '!',
+    code: 'Digit1'
+  },
+  {
+    key: '2',
+    shiftKey: '"',
+    code: 'Digit2'
+  },
+  {
+    key: '3',
+    shiftKey: '№',
+    code: 'Digit3'
+  },
+  {
+    key: '4',
+    shiftKey: ';',
+    code: 'Digit4'
+  },
+  {
+    key: '5',
+    shiftKey: '%',
+    code: 'Digit5'
+  },
+  {
+    key: '6',
+    shiftKey: ':',
+    code: 'Digit6'
+  },
+  {
+    key: '7',
+    shiftKey: '?',
+    code: 'Digit7'
+  },
+  {
+    key: '8',
+    shiftKey: '*',
+    code: 'Digit8'
+  },
+  {
+    key: '9',
+    shiftKey: '(',
+    code: 'Digit9'
+  },
+  {
+    key: '0',
+    shiftKey: ')',
+    code: 'Digit0'
+  },
+  {
+    key: '-',
+    shiftKey: '_',
+    code: 'Minus'
+  },
+  {
+    key: '=',
+    shiftKey: '+',
+    code: 'Equal'
+  },
+  {
+    key: 'Backspace',
+    shiftKey: null,
+    code: 'Backspace'
+  },
+  {
+    key: 'Tab',
+    shiftKey: null,
+    code: 'Tab'
+  },
+  {
+    key: 'й',
+    shiftKey: 'Й',
+    code: 'LetterQ'
+  },
+  {
+    key: 'ц',
+    shiftKey: 'Ц',
+    code: 'LetterW'
+  },
+  {
+    key: 'у',
+    shiftKey: 'У',
+    code: 'LetterE'
+  },
+  {
+    key: 'к',
+    shiftKey: 'К',
+    code: 'LetterR'
+  },
+  {
+    key: 'е',
+    shiftKey: 'Е',
+    code: 'LetterT'
+  },
+  {
+    key: 'н',
+    shiftKey: 'Н',
+    code: 'LetterY'
+  },
+  {
+    key: 'г',
+    shiftKey: 'Г',
+    code: 'LetterU'
+  },
+  {
+    key: 'ш',
+    shiftKey: 'Ш',
+    code: 'LetterI'
+  },
+  {
+    key: 'щ',
+    shiftKey: 'Щ',
+    code: 'LetterO'
+  },
+  {
+    key: 'з',
+    shiftKey: 'З',
+    code: 'LetterP'
+  },
+  {
+    key: 'х',
+    shiftKey: 'Х',
+    code: 'OpenBracket'
+  },
+  {
+    key: 'ъ',
+    shiftKey: 'Ъ',
+    code: 'CloseBracket'
+  },
+  {
+    key: '\\',
+    shiftKey: '/',
+    code: 'Backslash'
+  },
+  {
+    key: 'Del',
+    shiftKey: null,
+    code: 'Delete'
+  },
+  {
+    key: 'Caps Lock',
+    shiftKey: null,
+    code: 'CapsLock'
+  },
+  {
+    key: 'ф',
+    shiftKey: 'Ф',
+    code: 'LetterA'
+  },
+  {
+    key: 'ы',
+    shiftKey: 'Ы',
+    code: 'LetterS'
+  },
+  {
+    key: 'в',
+    shiftKey: 'В',
+    code: 'LetterD'
+  },
+  {
+    key: 'а',
+    shiftKey: 'А',
+    code: 'LetterF'
+  },
+  {
+    key: 'п',
+    shiftKey: 'П',
+    code: 'LetterG'
+  },
+  {
+    key: 'р',
+    shiftKey: 'Р',
+    code: 'LetterH'
+  },
+  {
+    key: 'о',
+    shiftKey: 'О',
+    code: 'LetterJ'
+  },
+  {
+    key: 'л',
+    shiftKey: 'Л',
+    code: 'LetterK'
+  },
+  {
+    key: 'д',
+    shiftKey: 'Д',
+    code: 'LetterL'
+  },
+  {
+    key: 'ж',
+    shiftKey: 'Ж',
+    code: 'Semicolon'
+  },
+  {
+    key: "э",
+    shiftKey: 'Э',
+    code: 'Quote'
+  },
+  {
+    key: 'Enter',
+    shiftKey: null,
+    code: 'Enter'
+  },
+  {
+    key: 'Shift',
+    shiftKey: null,
+    code: 'LeftShift'
+  },
+  {
+    key: 'я',
+    shiftKey: 'Я',
+    code: 'LetterZ'
+  },
+  {
+    key: 'ч',
+    shiftKey: 'Ч',
+    code: 'LetterX'
+  },
+  {
+    key: 'с',
+    shiftKey: 'С',
+    code: 'LetterC'
+  },
+  {
+    key: 'м',
+    shiftKey: 'М',
+    code: 'LetterV'
+  },
+  {
+    key: 'и',
+    shiftKey: 'И',
+    code: 'LetterB'
+  },
+  {
+    key: 'т',
+    shiftKey: 'Т',
+    code: 'LetterN'
+  },
+  {
+    key: 'ь',
+    shiftKey: 'Ь',
+    code: 'LetterM'
+  },
+  {
+    key: 'б',
+    shiftKey: 'Б',
+    code: 'Comma'
+  },
+  {
+    key: 'ю',
+    shiftKey: 'Ю',
+    code: 'Period'
+  },
+  {
+    key: '.',
+    shiftKey: ',',
+    code: 'ForwardSlash'
+  },
+  {
+    key: '&uarr;',
+    shiftKey: null,
+    code: 'ArrowUp'
+  },
+  {
+    key: 'Shift',
+    shiftKey: null,
+    code: 'RightShift'
+  },
+  {
+    key: 'Ctrl',
+    shiftKey: null,
+    code: 'LeftCtrl'
+  },
+  {
+    key: 'Win',
+    shiftKey: null,
+    code: 'Win'
+  },
+  {
+    key: 'Alt',
+    shiftKey: null,
+    code: 'LeftAlt'
+  },
+  {
+    key: ' ',
+    shiftKey: null,
+    code: 'Space'
+  },
+  {
+    key: 'Alt',
+    shiftKey: null,
+    code: 'RightAlt'
+  },
+  {
+    key: 'Ctrl',
+    shiftKey: null,
+    code: 'RightCtrl'
+  },
+  {
+    key: '&larr;',
+    shiftKey: null,
+    code: 'ArrowLeft'
+  },
+  {
+    key: '&darr;',
+    shiftKey: null,
+    code: 'ArrowDown'
+  },
+  {
+    key: '&rarr;',
+    shiftKey: null,
+    code: 'ArrowRight'
+  },
+];
+
+export { en, ru };
