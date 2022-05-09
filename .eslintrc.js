71db6014b34ea3ffa99fafe81d19b2da94a2dffb
marish11 <marishokola89@gmail.com>
2022-05-10 02:11:03 +0300

    refactor: adjust eslint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: 'airbnb-base',
-  parserOptions: {
-    ecmaVersion: 13,
-    sourceType: 'module',
-  },
-  rules: {
-    "indent": [error, 2],
-  },
-};
+    "env": {
+        "browser": true,
+        "node": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+}
